798ab74fd3f4799119ffe1d0fbbb82fbea560658
Jacob Carlborg <doob@me.com>
2013-10-30 22:22:50 +0100

    temp commit

--- build.sh
+++ build.sh
@@ -5,4 +5,12 @@ if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
     dvm use 2.063.2
 fi
 
-rdmd --build-only -debug -gc -ofbin/dstep -Idstack/mambo -Idstack -L-L. -L-lclang -L-ltango -L-rpath -L. "$@" dstep/driver/DStep.d
+rdmd --build-only -debug -gc -ofbin/dstep \
+	-Idstack/mambo -Idstack \
+	-L-L. \
+	-L-L/usr/lib/llvm-3.2/lib \
+	-L-lclang \
+	-L-ltango \
+	-L-rpath -L. \
+	-L-rpath -L/usr/lib/llvm-3.2/lib \
+	"$@" dstep/driver/DStep.d
